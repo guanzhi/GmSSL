95c0dba728e600aa39d176784d9c37587dc720fd
zs <84500316@qq.com>
2016-08-22 17:57:42 +0800

    补充（漏）提交说明见https://github.com/zsdev2015/GmSSL/commit/5c315bca3f08e706e6a81a133575cda315012e4d

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -2585,9 +2585,16 @@ EVP_PKEY *ssl_get_sign_pkey(SSL *s, const SSL_CIPHER *cipher,
             idx = SSL_PKEY_RSA_SIGN;
         else if (c->pkeys[SSL_PKEY_RSA_ENC].privatekey != NULL)
             idx = SSL_PKEY_RSA_ENC;
-    } else if ((alg_a & SSL_aECDSA) &&
+    } 
+# ifndef NO_GMSSL	
+	 else if ((alg_a & SSL_aSM2) &&
                (c->pkeys[SSL_PKEY_ECC].privatekey != NULL))
         idx = SSL_PKEY_ECC;
+# endif
+	else if ((alg_a & SSL_aECDSA) &&
+               (c->pkeys[SSL_PKEY_ECC].privatekey != NULL))
+        idx = SSL_PKEY_ECC;
+
     if (idx == -1) {
         SSLerr(SSL_F_SSL_GET_SIGN_PKEY, ERR_R_INTERNAL_ERROR);
         return (NULL);
