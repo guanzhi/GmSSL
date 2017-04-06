819729342340edf34ff9f5a94389bb067ee3975c
GGSuchao <1500062807@pku.edu.cn>
2017-04-06 12:25:05 +0800

    Update myserpent.h
    change the interface

--- myserpent.h
+++ myserpent.h
@@ -1,19 +1,102 @@
-#ifndef MYSERPENT_H 
-#define MYSERPENT_H
-unsigned char takebit(unsigned char bit_num);
-unsigned long int sb(char sb_num,unsigned long int sb_in_long);
-void linear(IN unsigned long int *li_0,IN unsigned long int *li_1,IN unsigned long int *li_2,IN unsigned long int *li_3,OUT unsigned long int *lo_0,OUT unsigned long int *lo_1,OUT unsigned long int *lo_2,OUT unsigned long int *lo_3);
-void IP(IN unsigned long int *ip_i0,IN unsigned long int *ip_i1,IN unsigned long int *ip_i2,IN unsigned long int *ip_i3,OUT unsigned long int *ip_o0,OUT unsigned long int *ip_o1,OUT unsigned long int *ip_o2,OUT unsigned long int *ip_o3);
-void FP(IN unsigned long int *fp_i0,IN unsigned long int *fp_i1,IN unsigned long int *fp_i2,IN unsigned long int *fp_i3,OUT unsigned long int *fp_o0,OUT unsigned long int *fp_o1,OUT unsigned long int *fp_o2,OUT unsigned long int *fp_o3);
-extern char sb0(char sb0_in);
-extern char sb1(char sb1_in);
-extern char sb2(char sb2_in);
-extern char sb3(char sb3_in);
-extern char sb4(char sb4_in);
-extern char sb5(char sb5_in);
-extern char sb6(char sb6_in);
-extern char sb7(char sb7_in);
-#define rotl(x,n)    (((x) << ((int)(n))) | ((x) >> (32 - (int)(n))))
-#define rotr(x,n)    (((x) >> ((int)(n))) | ((x) << (32 - (int)(n))))
+/**
+Copyright © 2015 Odzhan
+Copyright © 2008 Daniel Otte
+All Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY AUTHORS "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE. */
+
+#ifndef SERPENT_H
+#define SERPENT_H
+
+#include <stdint.h>
+
+#ifdef INTRINSICS
+#define memcpy(x,y,z) __movsb(x,y,z)
+#define memmove(x,y,z) __movsb(x,y,z)
+#define memset(x,y,z) __stosb(x,y,z)
+#define ROTL32(x,r) _rotl(x,r)
+#define ROTR32(x,r) _rotr(x,r)
+#else
+
+#define U8V(v)  ((uint8_t)(v)  & 0xFFU)
+#define U16V(v) ((uint16_t)(v) & 0xFFFFU)
+#define U32V(v) ((uint32_t)(v) & 0xFFFFFFFFUL)
+#define U64V(v) ((uint64_t)(v) & 0xFFFFFFFFFFFFFFFFULL)
+
+#define ROTL8(v, n) \
+  (U8V((v) << (n)) | ((v) >> (8 - (n))))
+
+#define ROTL16(v, n) \
+  (U16V((v) << (n)) | ((v) >> (16 - (n))))
+
+#define ROTL32(v, n) \
+  (U32V((v) << (n)) | ((v) >> (32 - (n))))
+
+#define ROTL64(v, n) \
+  (U64V((v) << (n)) | ((v) >> (64 - (n))))
+
+#define ROTR8(v, n) ROTL8(v, 8 - (n))
+#define ROTR16(v, n) ROTL16(v, 16 - (n))
+#define ROTR32(v, n) ROTL32(v, 32 - (n))
+#define ROTR64(v, n) ROTL64(v, 64 - (n))
+
+#endif
+
+#define GOLDEN_RATIO    0x9e3779b9l
+
+#define SERPENT_ROUNDS  32
+#define SERPENT_BLK_LEN 16
+#define SERPENT_KEY256  32
+
+#define SERPENT_ENCRYPT  0
+#define SERPENT_DECRYPT  1
+
+#define SERPENT_IP       0
+#define SERPENT_FP       1
+
+typedef union _serpent_blk_t {
+	uint8_t b[SERPENT_BLK_LEN];
+	uint32_t w[SERPENT_BLK_LEN / 4];
+	uint64_t q[SERPENT_BLK_LEN / 2];
+} serpent_blk;
+
+typedef uint32_t serpent_subkey_t[4];
+
+typedef struct  {
+	serpent_subkey_t x[SERPENT_ROUNDS + 1];
+} serpent_key_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	// C code
+	void serpent_set_encrypt_key(serpent_key_t *key, const unsigned char *user_key);
+	void serpent_encrypt(void *in, serpent_key_t *key);
+	void serpent_decrypt(void *in, serpent_key_t *key);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
