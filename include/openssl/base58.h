24b5ca96fd5f4212772c87360dc0293170f1bab9
GGSuchao <1500062807@pku.edu.cn>
2017-05-19 22:20:36 +0800

    Update base58.h

--- include/openssl/base58.h
+++ include/openssl/base58.h
@@ -53,7 +53,15 @@
 #include <stddef.h>
 #include <stdint.h>
 
-bool base58_decode(const char *b58, size_t b58sz, void *bin, size_t *binszp);
-bool base58_encode(const void *data, size_t binsz, char *b58, size_t *b58sz);
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+    int base58_decode(const char *in, size_t in_sz, char *out, size_t *out_szp);
+    int base58_encode(const char *in, size_t in_sz, char *out, size_t *out_sz);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
