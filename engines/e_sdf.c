0dcd7cf87ca5b035856a1b90457d7703e39a5707
guanzhi <guanzhi1980@gmail.com>
2017-05-20 18:05:44 +0800

    update engine

--- engines/e_sdf.c
+++ engines/e_sdf.c
@@ -1,5 +1,5 @@
 /* ====================================================================
- * Copyright (c) 2016 The GmSSL Project.  All rights reserved.
+ * Copyright (c) 2014 - 2017 The GmSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
