882a3e8a2fc6dbfaedbdefb7f9b18965be939b33
GGSuchao <1500062807@pku.edu.cn>
2017-06-25 14:02:40 +0800

    Update kdf_standard.h

--- include/openssl/kdf_standard.h
+++ include/openssl/kdf_standard.h
@@ -1,4 +1,23 @@
+/*
+ * Copyright 2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+
+#ifndef HEADER_KDF_STANDARD_H
+#define HEADER_KDF_STANDARD_H
+
 #include <string.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 #define SM3_len 256
 #define SM3_T1 0x79CC4519
@@ -379,3 +398,9 @@ void SM3_kdf(unsigned char Z[], unsigned short zlen, unsigned short klen, unsign
 		memcpy((K + SM2_NUMWORD * (t - 1)), Ha, SM2_NUMWORD);
 	}
 }
+
+
+#ifdef __cplusplus
+}
+# endif
+#endif
