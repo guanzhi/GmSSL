76752c7a9ddf55d268f7652bf9a173db287772bc
[GGSuchao] <[1500062807@pku.edu.cn]>
2017-07-06 18:14:28 +0800

    add sm2

--- include/openssl/mirdef.h
+++ include/openssl/mirdef.h
@@ -1,26 +1,41 @@
 /*
  *   MIRACL compiler/hardware definitions - mirdef.h
- *   For C++ build of library
+ *
+ *   This version is suitable for use with the GCC compiler
+ *   in a C/C++ only unix environment.
+ *   Remove mrmuldv.c from the build
+ *
+ *   For MS C on a PC, change BIG_ENDIAN to LITTLE_ENDIAN
+ *   and "long long" to "__int64"
+ *
+ *   Assembly language routines for the mrmuldv module will 
+ *   probably speed things up, so in most cases the generic 
+ *   mirdef.h32 file is appropriate
+ *
+ *   NOT recommended for Linux on PCs - read linux.txt
+ *
+ *   NOTE:- Read comments in miracl.mak unix make file
  */
 
-#ifndef HEADER_MIRDEF_H
+#ifdef HEADER_MIRDEF_H
 #define HEADER_MIRDEF_H
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-#define MR_LITTLE_ENDIAN
-#define MIRACL 64
-#define mr_utype long
-#define mr_unsign64 unsigned long
-#define MR_IBITS 32
-#define MR_LBITS 64
+
+#define MIRACL 32
+#define MR_BIG_ENDIAN    /* This may need to be changed        */
+#define mr_utype int
 #define mr_unsign32 unsigned int
+#define mr_dltype long long
+#define mr_unsign64 unsigned long long
+#define MR_IBITS 32
+#define MR_LBITS 32
+#define MR_NOASM
 #define MR_FLASH 52
 #define MAXBASE ((mr_small)1<<(MIRACL-1))
-#define MR_BITSINCHAR 8
-#define MR_CPP
 
 #ifdef __cplusplus
 }
