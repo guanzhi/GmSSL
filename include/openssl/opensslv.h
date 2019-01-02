2e0ff1f98e8af8408ec3a49f08e7d5d1945bbbb1
Zhi Guan <guanzhi1980@gmail.com>
2019-01-02 22:05:15 +0800

    Bug fix

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -41,9 +41,9 @@ extern "C" {
  */
 # define OPENSSL_VERSION_NUMBER  0x1010004fL
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "GmSSL 2.4.2 - OpenSSL 1.1.0d-fips  25 Dec 2018"
+#  define OPENSSL_VERSION_TEXT    "GmSSL 2.4.3 - OpenSSL 1.1.0d-fips  2 Jan 2019"
 # else
-#  define OPENSSL_VERSION_TEXT    "GmSSL 2.4.2 - OpenSSL 1.1.0d  25 Dec 2018"
+#  define OPENSSL_VERSION_TEXT    "GmSSL 2.4.3 - OpenSSL 1.1.0d  2 Jan 2019"
 # endif
 
 /*-
