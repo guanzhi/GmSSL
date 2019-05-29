317d1d54929c966ec6d1640b04702367069a2b9f
SophieK <35367649+suishixingkong@users.noreply.github.com>
2019-05-29 19:12:36 +0800

    Update ecschnorr.h
    fix a wrong

--- include/openssl/ecschnorr.h
+++ include/openssl/ecschnorr.h
@@ -78,5 +78,10 @@ int ECSCHNORR_sign(int type, const unsigned char *dgst, int dgstlen,
 int ECSCHNORR_verify(int type, const unsigned char *dgst, int dgstlen,
 	const unsigned char *sig, int siglen, EC_KEY *ec_key);
 
+#ifdef __cplusplus
+}
+#endif
 
+#endif  /* OPENSSL_NO_ECSCHNORR */
+#endif  /* HEADER_ECSCHNORR_H */
 /* BEGIN ERROR CODES */
