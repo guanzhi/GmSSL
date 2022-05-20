252839e1968b48192b2a9d57f995125936c59943
Zhi Guan <guanzhi1980@gmail.com>
2022-05-20 17:00:30 +0800

    Update mem.h

--- include/gmssl/mem.h
+++ include/gmssl/mem.h
@@ -49,6 +49,7 @@
 #ifndef GMSSL_MEM_H
 #define GMSSL_MEM_H
 
+#include <stdint.h>
 #include <stddef.h> // where size_t from
 
 
@@ -58,8 +59,7 @@ void gmssl_memxor(void *r, const void *a, const void *b, size_t len);
 int gmssl_secure_memcmp(const volatile void * volatile in_a, const volatile void * volatile in_b, size_t len);
 void gmssl_secure_clear(void *ptr, size_t len);
 
-int mem_is_zero(const uint8_t *buf, size_t len);
-
+int mem_is_zero(const uint8_t *buf, size_t len); // FIXME: uint8_t * to void *
 
 #endif
 
