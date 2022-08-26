4e5880e2ea4e715a90590049ae4570a0bb1f598a
Simon <zhaopku09@gmail.com>
2022-08-26 13:44:26 +0800

    Revert "修改License"
    This reverts commit 0b4308b948aa29e30208cd116b2989dbdd87714d.

--- include/gmssl/rand.h
+++ include/gmssl/rand.h
@@ -1,12 +1,11 @@
 /*
- *  Copyright 2014-2022 The GmSSL Project. All Rights Reserved.
+ *  Copyright 2022 The GmSSL Project. All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the License); you may
  *  not use this file except in compliance with the License.
  *
  *  http://www.apache.org/licenses/LICENSE-2.0
  */
-
 
 
 #ifndef GMSSL_RAND_H
