34af2f48096f354c9a1e3421d4f32922546922a1
Zhi Guan <guanzhi1980@gmail.com>
2023-12-17 21:10:20 +0800

    Update sm3hmac.c
    Add `-in_str` option

--- tools/sm3hmac.c
+++ tools/sm3hmac.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2014-2022 The GmSSL Project. All Rights Reserved.
+ *  Copyright 2014-2023 The GmSSL Project. All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the License); you may
  *  not use this file except in compliance with the License.
@@ -17,22 +17,52 @@
 #include <gmssl/sm3.h>
 
 
-static const char *options = "-key hex [-in file] [-bin|-hex] [-out file]";
+static const char *usage = "-key hex [-in file | -in_str str] [-bin|-hex] [-out file]";
+
+static const char *help =
+"Options\n"
+"\n"
+"    -key hex               Hex string of the MAC key\n"
+"    -in_str str            Input as text string\n"
+"    -in file | stdin       Input file path\n"
+"                           `-in_str` and `-in` should not be used together\n"
+"                           If neither `-in` nor `-in_str` specified, read from stdin\n"
+"    -hex                   Output MAC-tag as hex string (by default)\n"
+"    -bin                   Output MAC-tag as binary\n"
+"                           `-hex` and `-bin` should not be used together\n"
+"    -out file | stdout     Output file path. If not specified, output to stdout\n"
+"\n"
+"Examples\n"
+"\n"
+"    KEY_HEX=`gmssl rand -outlen 16 -hex`\n"
+"    gmssl sm3hmac -key $KEY_HEX -in_str abc\n"
+"\n"
+"    gmssl sm3hmac -key $KEY_HEX -in_str abc -bin\n"
+"\n"
+"    gmssl sm3hmac -key $KEY_HEX -in /path/to/file\n"
+"\n"
+"  When reading from stdin, make sure the trailing newline character is removed\n"
+"\n"
+"  Linux/Mac:\n"
+"    echo -n abc | gmssl sm3hmac -key $KEY_HEX\n"
+"\n"
+"  Windows:\n"
+"    C:\\> echo |set/p=\"abc\" | gmssl sm3hmac -key 11223344556677881122334455667788\n"
+"\n";
 
 int sm3hmac_main(int argc, char **argv)
 {
 	int ret = 1;
 	char *prog = argv[0];
 	char *keyhex = NULL;
-	int bin = 0;
+	int outformat = 0;
+	char *in_str = NULL;
 	char *infile = NULL;
 	char *outfile = NULL;
 	uint8_t key[SM3_DIGEST_SIZE];
 	size_t keylen;
 	FILE *infp = stdin;
 	FILE *outfp = stdout;
-	uint8_t buf[4096];
-	size_t len;
 	SM3_HMAC_CTX ctx;
 	uint8_t mac[SM3_HMAC_SIZE];
 	size_t i;
@@ -41,13 +71,14 @@ int sm3hmac_main(int argc, char **argv)
 	argv++;
 
 	if (argc < 1) {
-		fprintf(stderr, "usage: %s %s\n", prog, options);
+		fprintf(stderr, "usage: %s %s\n", prog, usage);
 		return 1;
 	}
 
 	while (argc > 0) {
 		if (!strcmp(*argv, "-help")) {
-			printf("usage: %s %s\n", prog, options);
+			printf("usage: %s %s\n", prog, usage);
+			printf("%s\n", help);
 			ret = 0;
 			goto end;
 		} else if (!strcmp(*argv, "-key")) {
@@ -62,9 +93,24 @@ int sm3hmac_main(int argc, char **argv)
 				goto end;
 			}
 		} else if (!strcmp(*argv, "-hex")) {
-			bin = 0;
+			if (outformat == 2) {
+				fprintf(stderr, "%s: `-hex` and `-bin` should not be used together\n", prog);
+				goto end;
+			}
+			outformat = 1;
 		} else if (!strcmp(*argv, "-bin")) {
-			bin = 1;
+			if (outformat == 1) {
+				fprintf(stderr, "%s: `-hex` and `-bin` should not be used together\n", prog);
+				goto end;
+			}
+			outformat = 2;
+		} else if (!strcmp(*argv, "-in_str")) {
+			if (infile) {
+				fprintf(stderr, "%s: `-in` and `-in_str` should not be used together\n", prog);
+				goto end;
+			}
+			if (--argc < 1) goto bad;
+			in_str = *(++argv);
 		} else if (!strcmp(*argv, "-in")) {
 			if (--argc < 1) goto bad;
 			infile = *(++argv);
@@ -97,12 +143,19 @@ bad:
 	}
 
 	sm3_hmac_init(&ctx, key, keylen);
-	while ((len = fread(buf, 1, sizeof(buf), infp)) > 0) {
-		sm3_hmac_update(&ctx, buf, len);
+	if (in_str) {
+		sm3_hmac_update(&ctx, (uint8_t *)in_str, strlen(in_str));
+	} else {
+		uint8_t buf[4096];
+		size_t len;
+		while ((len = fread(buf, 1, sizeof(buf), infp)) > 0) {
+			sm3_hmac_update(&ctx, buf, len);
+		}
+		memset(buf, 0, sizeof(buf));
 	}
 	sm3_hmac_finish(&ctx, mac);
 
-	if (bin) {
+	if (outformat > 1) {
 		if (fwrite(mac, 1, sizeof(mac), outfp) != sizeof(mac)) {
 			fprintf(stderr, "%s: output failure : %s\n", prog, strerror(errno));
 			goto end;
