1def7529481a77112c6afa17f56de177fa0551fd
Zhi Guan <guanzhi1980@gmail.com>
2023-12-25 10:26:20 +0800

    Update sm4_cbc_mac.h

--- include/gmssl/sm4_cbc_mac.h
+++ include/gmssl/sm4_cbc_mac.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2014-2022 The GmSSL Project. All Rights Reserved.
+ *  Copyright 2014-2023 The GmSSL Project. All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the License); you may
  *  not use this file except in compliance with the License.
@@ -23,6 +23,8 @@ typedef struct {
 	uint8_t iv[16];
 	size_t ivlen;
 } SM4_CBC_MAC_CTX;
+
+#define SM4_CBC_MAC_SIZE	(SM4_BLOCK_SIZE)
 
 void sm4_cbc_mac_init(SM4_CBC_MAC_CTX *ctx, const uint8_t key[16]);
 void sm4_cbc_mac_update(SM4_CBC_MAC_CTX *ctx, const uint8_t *data, size_t datalen);
