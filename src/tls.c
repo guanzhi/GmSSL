fca6924e28e9aa5da7baf5e4dd16fbb1916af323
Simon <zhaopku09@gmail.com>
2024-04-20 12:18:44 +0800

    Update tls.c

--- src/tls.c
+++ src/tls.c
@@ -2321,7 +2321,7 @@ int tls_set_socket(TLS_CONNECT *conn, tls_socket_t sock)
 {
 	int flags;
 #ifdef WIN32
-    	if( ioctlsocket(sockfd,FIONBIO,&flags) != 0)   {
+    	if( ioctlsocket(sock, FIONBIO, &flags) != 0)   {
 		error_puts("socket in non-blocking mode");
 		//nginx will pass a socket in non-blocking mode
 		//return -1;
@@ -2332,7 +2332,6 @@ int tls_set_socket(TLS_CONNECT *conn, tls_socket_t sock)
 		perror("fcntl error");
 		return -1;
 	}
-
 	if (flags & O_NONBLOCK) {
 		error_puts("socket in non-blocking mode");
 		//nginx will pass a socket in non-blocking mode
