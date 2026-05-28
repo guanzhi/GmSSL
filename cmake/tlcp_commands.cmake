d90e7638fb59bffc15f0b333a3591a638bb1b6ec
Zhi Guan <guanzhi1980@gmail.com>
2026-05-28 09:17:20 +0800

    Update workflow

--- cmake/tlcp_commands.cmake
+++ cmake/tlcp_commands.cmake
@@ -43,25 +43,25 @@ if(NOT ${SERVER_RESULT} EQUAL 0)
 	message(FATAL_ERROR "server failed to start")
 endif()
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 2)
-
-
-if (WIN32)
-	execute_process(
-		COMMAND cmd /c "start /B bin\\gmssl tlcp_client -host localhost -port ${TLCP_TEST_PORT} -cacert rootcacert.pem > tlcp_client.log 2>&1"
-		RESULT_VARIABLE CLIENT_RESULT
-		TIMEOUT 5
-	)
-else()
-	execute_process(
-		COMMAND bash -c "bin/gmssl tlcp_client -host localhost -port ${TLCP_TEST_PORT} -cacert rootcacert.pem < /dev/null > tlcp_client.log 2>&1 &"
-		RESULT_VARIABLE CLIENT_RESULT
-		TIMEOUT 5
-	)
-endif()
-
 set(FOUND_INDEX -1)
 foreach(i RANGE 1 15)
+	if (WIN32)
+		execute_process(
+			COMMAND cmd /c "start /B bin\\gmssl tlcp_client -host 127.0.0.1 -port ${TLCP_TEST_PORT} -cacert rootcacert.pem > tlcp_client.log 2>&1"
+			RESULT_VARIABLE CLIENT_RESULT
+			TIMEOUT 5
+		)
+	else()
+		execute_process(
+			COMMAND bash -c "bin/gmssl tlcp_client -host 127.0.0.1 -port ${TLCP_TEST_PORT} -cacert rootcacert.pem < /dev/null > tlcp_client.log 2>&1 &"
+			RESULT_VARIABLE CLIENT_RESULT
+			TIMEOUT 5
+		)
+	endif()
+	if(NOT ${CLIENT_RESULT} EQUAL 0)
+		message(FATAL_ERROR "client failed to start")
+	endif()
+	execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 1)
 	if(EXISTS "tlcp_client.log")
 		file(READ "tlcp_client.log" CLIENT_LOG_CONTENT)
 		string(FIND "${CLIENT_LOG_CONTENT}" "Connection established" FOUND_INDEX)
@@ -69,7 +69,6 @@ foreach(i RANGE 1 15)
 			break()
 		endif()
 	endif()
-	execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 1)
 endforeach()
 
 if(NOT WIN32)
@@ -86,5 +85,13 @@ if(NOT WIN32)
 endif()
 
 if(${FOUND_INDEX} EQUAL -1)
+	if(EXISTS "tlcp_server.log")
+		file(READ "tlcp_server.log" SERVER_LOG_CONTENT)
+		message(STATUS "tlcp_server.log:\n${SERVER_LOG_CONTENT}")
+	endif()
+	if(EXISTS "tlcp_client.log")
+		file(READ "tlcp_client.log" CLIENT_LOG_CONTENT)
+		message(STATUS "tlcp_client.log:\n${CLIENT_LOG_CONTENT}")
+	endif()
 	message(FATAL_ERROR "Client did not establish connection with server.")
 endif()
